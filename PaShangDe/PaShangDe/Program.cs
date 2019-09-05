de580bde4c38626df92b4ac883827b9b58e5dbd9
晨 李 <262126479@qq.com>
2019-09-05 14:03:57 +0800

    首次提交

---
+++ PaShangDe/PaShangDe/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaShangDe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var http = new HttpClient();
+            Task.Run(async () => 
+            {
+                for (int i = 1; i < 1500; i++)
+                {
+                    string pageData = http.GetAsync(@"http://sfs-public-1257236099.cos.ap-beijing.myqcloud.com/teach-resource/resource/726/DatumBundle/"
+                        + i +
+                        @"/【1910密训资料】数据库系统原理（全国）.pdf").Result.StatusCode.ToString();
+                    if (pageData != "NotFound")
+                    {
+                        Console.WriteLine($"{i}   " + pageData);
+                    }
+                }
+            }).GetAwaiter().GetResult();
+
+            Console.WriteLine("完成了");
+            Console.ReadKey();
+        }
+    }
+}
